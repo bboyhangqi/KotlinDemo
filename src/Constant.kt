bfda23ea66351c1fe04410d44f47d4b13b48f5a9
zhaomingming-s <zhaomingming-s@360.cn>
2017-08-18 15:40:08 +0800

    modify the project structure

--- src/Constant.kt
+++
@@ -1,9 +0,0 @@
-/**
- * Created by CodingHome on 2017/6/9.
- */
-object Constant {
-
-
-    const  val MALE = 0
-    const  val FEMALE = 1
-}
--- src/Human.kt
+++
@@ -1,7 +0,0 @@
-/**
- * Created by CodingHome on 2017/6/9.
- */
-interface Human {
-
-
-}
